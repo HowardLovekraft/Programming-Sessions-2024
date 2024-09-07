375e37c903c3c7823dac6753f57979a21e32cdbd
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2024-09-07 21:32:34 +0500

    добавил домашку к 5 занятию
    > +1 задачка "на дом" к 5 сессии
    > Обновил презентацию 4 сессии (была неправильная)
    > Добавил презентацию 5 сессии (спойлеры-спойлеры)(

---
+++ Sessions/session_04/home_tasks/task01.py
@@ -0,0 +1,14 @@
+import math
+def percentile_of_seq(seq: list[int], percentile: int) -> list:
+    """
+    Функция возвращает значения из N-ого процентиля последовательности, т.е. первые N% значений из отсортированной последовательности. Подробнее про процентиль - на википедии.
+        :param listy: Последовательность, для которой вычисляется квантиль
+        :param percentile: Номер процентиля (от 1 до 99)
+        :return result: Список - числа, входящие в N-ый процентиль
+    """
+    sorted_seq = sorted(seq)
+    seq_len = len(sorted_seq)
+    percentile_len = math.ceil(seq_len * 0.01 * percentile)
+    result = sorted_seq[:percentile_len]
+
+print(percentile_of_seq([1, 2, 3, 4, 5], 40)) # В консоли должно появиться "[1,2]"
